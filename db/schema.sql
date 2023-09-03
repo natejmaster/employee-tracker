109447b903f8e3fdae22ee813930c6d7892223ea
natejmaster@gmail.com <natejmaster@gmail.com>
2023-09-03 07:39:13 -0500

    Added reference to employee id for manager id value

--- db/schema.sql
+++ db/schema.sql
@@ -26,5 +26,8 @@ CREATE TABLE employees (
     manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id)
+    REFERENCES employees(employee_id)
     ON DELETE SET NULL
 )
