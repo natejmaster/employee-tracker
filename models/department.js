05df93a37232d33bf74daea75c354e7ebd296035
natejmaster@gmail.com <natejmaster@gmail.com>
2023-09-08 10:00:22 -0500

    Finalized functions for view all departments and add department

--- models/department.js
+++ models/department.js
@@ -1,3 +1,4 @@
+const inquirer = require('inquirer');
 const connection = require('../dbConnection');
 const { table } = require('table'); // You can use a table formatting library if desired
 
@@ -21,4 +22,46 @@ function viewDepartments() {
     });
 }
 
-module.exports = { viewDepartments };
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'department_name',
+            message: 'Enter the name of the new department:',
+            validate: function (value) {
+                if (value.trim() !=='') {
+                    return true;
+                }
+                return 'Department name cannot be empty.';
+            },
+        },
+    ])
+    .then((answers) => {
+        const queryCheck = 'SELECT * FROM departments WHERE department_name = ?';
+        const checkValues = [answers.department_name];
+
+        connection.query(queryCheck, checkValues, (err,results) => {
+            if (err) {
+                console.error('Error checking for existing department', err);
+                return;
+            }
+            if (results.length > 0) {
+                console.log(`\nThe department '${answers.department_name}' already exists.`);
+                return;
+            }
+            const queryInsert = 'INSERT INTO departments (department_name) VALUES (?)';
+            const insertValues = [answers.department_name];
+
+            connection.query(queryInsert, insertValues, (err, result) => {
+                if (err) {
+                    console.error('Error adding department', err);
+                    return;
+                }
+            console.log(`\n${answers.department_name} department has been added successfully.`);
+            viewDepartments(); // Optionally, view all departments after adding
+            });
+        });
+    });
+}
+
+module.exports = { viewDepartments, addDepartment };
